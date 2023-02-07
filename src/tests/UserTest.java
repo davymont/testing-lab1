6d0b1fe9d98669ab01dcaf5f5d31ffba048de173
David Montgomery <davemont@outlook.com>
2023-02-07 22:04:52 +0100

    Up to 3.3.2 in doc

--- src/tests/UserTest.java
+++ src/tests/UserTest.java
@@ -4,14 +4,44 @@ import model.User;
 
 public class UserTest {
     public static void main(String[] args) {
+//        User testUser = new User(
+//                "mike",
+//                "my_passwd",
+//                "Mike",
+//                "Smith",
+//                "07771234567"
+//        );
+//
+//        System.out.println(testUser);
+
+        String test_username = "mike";
+        String test_password = "my_passwd";
+        String test_first_name = "Mike";
+        String test_last_name = "Smith";
+        String test_mobile_number = "07771234567";
+
         User testUser = new User(
-                "mike",
-                "my_passwd",
-                "Mike",
-                "Smith",
-                "07771234567"
+                test_username,
+                test_password,
+                test_first_name,
+                test_last_name,
+                test_mobile_number
         );
 
-        System.out.println(testUser);
+        Boolean passed = true;
+
+        if (testUser.getUsername() != test_username) {
+            System.out.println("TC1 failed: username did not match");
+            passed = false;
+        }
+        if (testUser.getPassword() != test_password) {
+            System.out.println("TC2 failed: password did not match");
+            passed = false;
+        }
+
+        if (passed) {
+            System.out.println("All TC's passed.");
+        }
+
     }
 }
